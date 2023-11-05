7399f29373f2fc87a54f693ad69e77cadce88961
Gabor Biro <gabor.biro@yahoo.com>
2023-11-05 12:45:12 +0000

    Fixed compiletimeconstants

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.anatawa12.compileTimeConstant.CreateConstantsTask
 import app.gaborbiro.flathunt.Dependencies
 
 plugins {
     kotlin("jvm")
     id("com.google.devtools.ksp")
+    id("com.anatawa12.compile-time-constant")
 }
 
 apply(from = "${project.rootDir}/services.gradle")
@@ -26,4 +28,11 @@ dependencies {
     implementation(Dependencies.Koin.annotations)
     ksp(Dependencies.Koin.ksp)
     implementation(Dependencies.OkHttp.okhttp)
+}
+
+val createCompileTimeConstant = tasks.withType(CreateConstantsTask::class.java) {
+    constantsClass = "app.gaborbiro.flathunt.compileTimeConstant.Constants"
+    values(
+        (ext["SERVICE_CONSTANTS"] as ArrayList<String>).associateBy { it }
+    )
 }
